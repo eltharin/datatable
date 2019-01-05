1c2dc360ac58daa85d50bdb1df5e4323566508f0
eltharin <eltharin18@hotmail.fr>
2019-01-05 20:19:05 +0100

    update for new HTMLTemplate

--- Classes/Functions.php
+++ Classes/Functions.php
@@ -6,24 +6,24 @@ class Functions
 {
 	static function includes()
 	{
-		\config::add_css('/datatable/css/datatable/filter.datatable.css');
-		\config::add_css('/datatable/css/datatable/datatable.css');
-		\config::add_css('/datatable/css/datatable/pager.datatable.css');
-		\config::add_css('/datatable/css/datatable/menuLength.datatable.css');
-		\config::add_css('/datatable/css/datatable/output.datatable.css');
-		\config::add_css('/datatable/css/multiple-select.css');
+		\Core::$config->HTMLtemplate->add_css('/datatable/css/datatable/filter.datatable.css');
+		\Core::$config->HTMLtemplate->add_css('/datatable/css/datatable/datatable.css');
+		\Core::$config->HTMLtemplate->add_css('/datatable/css/datatable/pager.datatable.css');
+		\Core::$config->HTMLtemplate->add_css('/datatable/css/datatable/menuLength.datatable.css');
+		\Core::$config->HTMLtemplate->add_css('/datatable/css/datatable/output.datatable.css');
+		\Core::$config->HTMLtemplate->add_css('/datatable/css/multiple-select.css');
 
-		\config::add_script('/datatable/js/datatable/core/jquery.dataTables.js'); 
-		\config::add_script('/datatable/js/datatable/renderers.datatable.js');
-		\config::add_script('/datatable/js/datatable/dataTable.basic.js'); 
+		\Core::$config->HTMLtemplate->add_script('/datatable/js/datatable/core/jquery.dataTables.js'); 
+		\Core::$config->HTMLtemplate->add_script('/datatable/js/datatable/renderers.datatable.js');
+		\Core::$config->HTMLtemplate->add_script('/datatable/js/datatable/dataTable.basic.js'); 
 		
-		\config::add_script('/datatable/js/datatable/widget/widget-pager.datatable.js'); 
-		\config::add_script('/datatable/js/datatable/widget/widget-menuLength.datatable.js'); 
-		\config::add_script('/datatable/js/datatable/widget/widget-json.datatable.js'); 
-		\config::add_script('/datatable/js/datatable/widget/widget-filter.datatable.js'); 
-		\config::add_script('/datatable/js/datatable/widget/widget-totalizer.datatable.js');
-		\config::add_script('/datatable/js/datatable/widget/widget-numColFooter.datatable.js');
-		\config::add_script('/datatable/js/datatable/widget/widget-output.datatable.js'); 
-		\config::add_script('/datatable/js/multiple-select.js');
+		\Core::$config->HTMLtemplate->add_script('/datatable/js/datatable/widget/widget-pager.datatable.js'); 
+		\Core::$config->HTMLtemplate->add_script('/datatable/js/datatable/widget/widget-menuLength.datatable.js'); 
+		\Core::$config->HTMLtemplate->add_script('/datatable/js/datatable/widget/widget-json.datatable.js'); 
+		\Core::$config->HTMLtemplate->add_script('/datatable/js/datatable/widget/widget-filter.datatable.js'); 
+		\Core::$config->HTMLtemplate->add_script('/datatable/js/datatable/widget/widget-totalizer.datatable.js');
+		\Core::$config->HTMLtemplate->add_script('/datatable/js/datatable/widget/widget-numColFooter.datatable.js');
+		\Core::$config->HTMLtemplate->add_script('/datatable/js/datatable/widget/widget-output.datatable.js'); 
+		\Core::$config->HTMLtemplate->add_script('/datatable/js/multiple-select.js');
 	}
 }
