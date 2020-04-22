8b5d77182db191fdb6d45c9b8e9c49f24cf56d4d
Eltharin <eltharin18@hotmail.fr>
2020-04-22 13:13:57 +0200

    start after doc

--- Files/js/widget/widget-form.datatable.js
+++ Files/js/widget/widget-form.datatable.js
@@ -1,19 +1,20 @@
+$(document).ready(function(){
+	$('.dt-form').on('submit',function(){
 
-$('.dt-form').on('submit',function(){
-
-	var thisform = $(this);
-	$('#'+thisform.data('tableid')).DataTable().$('input, select').each(function(index, data)
-	{
-		if($(data).prop('type') === 'checkbox')
+		var thisform = $(this);
+		$('#'+thisform.data('tableid')).DataTable().$('input, select').each(function(index, data)
 		{
-			if($(data).prop('checked'))
+			if($(data).prop('type') === 'checkbox')
+			{
+				if($(data).prop('checked'))
+				{
+					thisform.prepend('<input type="hidden" value="'+$(data).val()+'" name="'+$(data).attr('name')+'">');
+				}
+			}
+			else
 			{
 				thisform.prepend('<input type="hidden" value="'+$(data).val()+'" name="'+$(data).attr('name')+'">');
 			}
-		}
-		else
-		{
-			thisform.prepend('<input type="hidden" value="'+$(data).val()+'" name="'+$(data).attr('name')+'">');
-		}
+		});
 	});
 });
